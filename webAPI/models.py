a62940cbf7f707b331467f5bced292e4e41c4bce
theOpetunde <opetundeadepoju@gmail.com>
2020-02-27 14:09:57 +0100

    Created Project and Action models in models.py

--- webAPI/models.py
+++ webAPI/models.py
@@ -5,3 +5,10 @@ class Project(models.Model):
     name = models.CharField(max_length=255, required=True)
     description = models.TextField(required=True)
     completed = models.BooleanField()
+
+
+class Action(models.Model):
+    project_id = models.ForeignKey(Project, On_delete=models.CASCADE)
+    description = models.CharField(max_length = 128, required=True)
+    notes = models.TextField(required=True)
+    completed = models.BooleanField()
